ed74fe19476159eb31f828644d52ea33bfa3768e
shino-kyutech <shino.kyutech@gmail.com>
2025-05-08 13:50:08 +0900

    autoexec_all_pubmed23.sh の検索のパラメタの修正

--- autoexec_all_pubmed23.sh
+++ autoexec_all_pubmed23.sh
@@ -13,7 +13,6 @@
 ./autoexec_make_bucket.sh sketch_w21
 
 # search by double filtering
-mkdir temp
-./search_by_double_filtering_pubmed23.sh sketch_w21 QPSMAP_p192 00_23 otest NONE NONE temp 30 0 2 2.0 0.5 10 25 8 7 13 3 1.05 100 0 8 1 1 df_parameters_w21_p192_2bit.txt summary.csv NONE
+./search_by_double_filtering_pubmed23.sh sketch_w21 QPSMAP_p192 00_23 otest NONE NONE result 30 0 2 2.0 0.5 10 25 8 7 13 3 1.05 100 0 8 1 1 df_parameters_w21_p192_2bit.txt summary.csv NONE
 #./search_by_double_filtering_pubmed23.sh sketch_w21 QPSMAP_p192 00_23 otest NONE NONE result.csv 30 0 2 2.0 0.5 10 25 8 7 13 3 1.05 100 0 8 1 1 df_parameters_w21_p192_2bit.txt result/summary.csv result/search_cost.csv
 
