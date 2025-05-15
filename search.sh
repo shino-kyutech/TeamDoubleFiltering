d371e0376e86e985799ade300ab9b28273a8d8a1
shino-kyutech <shino.kyutech@gmail.com>
2025-05-15 11:03:09 +0900

    modify search.sh, and add pivot for QPSMAP of 384-dim and batch_for_all.txt

--- search.sh
+++ search.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-if [ $# -ne 5 ] ; then
-echo "Usage>"
+if [ $# -ne 6 ] ; then
+echo "Usage> ./search.sh parms:"
 echo "1.  width      = width of pivot for 1st filtering"
-echo "2.  rs_dir     = directory for result files"
-echo "3.  q_bit      = quantization bit of QPSMAP" 
-echo "4.  FTR_ON     = ftr data on: 0 -> Second memory, 1 -> RAM"
-echo "5.  batch_file = parameter file (nc1 and nc2)"
+echo "2.  smap dim.  = dimension of smap"
+echo "3.  rs_dir     = directory for result files"
+echo "4.  q_bit      = quantization bit of QPSMAP" 
+echo "5.  FTR_ON     = ftr data on: 0 -> Second memory, 1 -> RAM"
+echo "6.  batch_file = parameter file (nc1 and nc2)"
 
 exit 1
 fi
 
 width=$1; shift
+smap=$1; shift
 rs_dir=$1; shift
 qbit=$1; shift
 ftr=$1; shift
@@ -30,4 +32,4 @@ fi
 
 echo $summary
 
-./search_by_double_filtering.sh sketch_w$width QPSMAP_p192 00_23 otest NONE NONE $rs_dir 30 0 $qbit 2.0 0.5 10 25 8 7 13 3 1.05 100 0 8 $ftr 1 $batch $summary NONE
+./search_by_double_filtering.sh sketch_w$width QPSMAP_p$smap 00_23 otest NONE NONE $rs_dir 30 0 $qbit 2.0 0.5 10 25 8 7 13 3 1.05 100 0 8 $ftr 1 $batch $summary NONE
