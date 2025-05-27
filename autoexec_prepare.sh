9efdde402e272760f1d92b953d71d532340edb8c
shino-kyutech <shino.kyutech@gmail.com>
2025-05-27 16:12:07 +0900

    modify .sh to measure executing time

--- autoexec_prepare.sh
+++ autoexec_prepare.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # convert datset of h5 to ftr 
-./autoexec_convert_dataset.sh
+time ./autoexec_convert_dataset.sh
 
 # convert otest to ftr
-./autoexec_convert_otest.sh
+time ./autoexec_convert_otest.sh
 
 # convert ground truth to answer file
-./autoexec_gt_otest_to_answer.sh
+time ./autoexec_gt_otest_to_answer.sh
 
 # make bucket file for sketch
-./autoexec_make_bucket.sh sketch_w16
-./autoexec_make_bucket.sh sketch_w18
-./autoexec_make_bucket.sh sketch_w20
+time ./autoexec_make_bucket.sh sketch_w16
+time ./autoexec_make_bucket.sh sketch_w18
+time ./autoexec_make_bucket.sh sketch_w20
